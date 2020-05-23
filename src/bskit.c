9cf5424ab50dda550131b0c84b08d0ff5fc1e448
Anirudh Bezzam <abezzam@091-EQCORPML25.fios-router.home>
2020-05-23 19:07:31 -0400

    Add switch cases to stack to prevent iteration

--- src/bskit.c
+++ src/bskit.c
@@ -21,3 +21,49 @@ const int program[] = {
     POP,
     HLT
 };
+
+int fetch() {
+    return program[ip];
+}
+
+void eval(int instr) {
+    switch (instr) {
+        case HLT: {
+            running = false;
+            printf("done\n");
+            break;
+        }
+        case PSH: {
+    	    sp++;
+	        stack[sp] = program[++ip];
+	        break;
+        }
+        case POP: {
+	        int val_popped = stack[sp--];
+	        printf("popped %d\n", val_popped);
+	        break;
+	    }
+	    case ADD: {
+	        // first we pop the stack and store it as a
+	        int a = stack[sp--];
+	    
+	        // then we pop the top of the stack and store it as b
+	        int b = stack[sp--];
+
+	        // we then add the result and push it to the stack
+	        int result = b + a;
+	        sp++; // increment stack pointer **before**
+	        stack[sp] = result; // set the value to the top of the stack
+
+	        // all done!
+	        break;
+	    }
+    }
+}
+
+int main() {
+    while (running) {
+        eval(fetch());
+        ip++; // increment the ip every iteration
+    }
+}
